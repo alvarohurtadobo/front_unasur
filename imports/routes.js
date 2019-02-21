025d12cc3763636d4f82824bff4e4fc734621837
chaskynet <anibal.zapata.agreda@gmail.com>
2019-02-21 14:27:50 -0400

    Modificacion de ruta en los routes

--- imports/routes.js
+++ imports/routes.js
@@ -1,39 +1,39 @@
 import { RouterFactory } from 'meteor/akryum:vue-router2'
 
 // Components
-import home from '/imports/ui/Home.vue';
-import NotFound from '/imports/ui/NotFound.vue';
+import home from './ui/Home.vue';
+import NotFound from './ui/NotFound.vue';
 // ************* SALA MULTIPLE ***********
-import sm from "/imports/ui/bloques/sala_multiple/Opt_sm.vue";
-import accesos_sm_detalle from "/imports/ui/bloques/sala_multiple/Accesos_sm.vue";
-import iluminacion_sm_convensiones from "/imports/ui/bloques/sala_multiple/ilum_sm_convensiones.vue";
-import iluminacion_sm_hall from "/imports/ui/bloques/sala_multiple/ilum_sm_hall.vue";
-import iluminacion_sm_parqueo from "/imports/ui/bloques/sala_multiple/ilum_sm_parqueo.vue";
-import energia_sm from '/imports/ui/bloques/sala_multiple/energia_sm.vue';
-import riego from '/imports/ui/bloques/sala_multiple/riego_sm.vue';
-import camaras_sm from '/imports/ui/bloques/sala_multiple/camaras_sm.vue';
+import sm from "./ui/bloques/sala_multiple/Opt_sm.vue";
+import accesos_sm_detalle from "./ui/bloques/sala_multiple/Accesos_sm.vue";
+import iluminacion_sm_convensiones from "./ui/bloques/sala_multiple/ilum_sm_convensiones.vue";
+import iluminacion_sm_hall from "./ui/bloques/sala_multiple/ilum_sm_hall.vue";
+import iluminacion_sm_parqueo from "./ui/bloques/sala_multiple/ilum_sm_parqueo.vue";
+import energia_sm from './ui/bloques/sala_multiple/energia_sm.vue';
+import riego from './ui/bloques/sala_multiple/riego_sm.vue';
+import camaras_sm from './ui/bloques/sala_multiple/camaras_sm.vue';
 // ************* PLAZA DE COMIDAS ************
-import pc from '/imports/ui/bloques/plaza_comidas/Opt_pc.vue';
-import accesos_pc from "/imports/ui/bloques/plaza_comidas/Accesos_pc.vue";
-import iluminacion_pc from '/imports/ui/bloques/plaza_comidas/Ilum_pc.vue';
-import incendio_pc from '/imports/ui/bloques/plaza_comidas/Incendio_pc.vue';
+import pc from './ui/bloques/plaza_comidas/Opt_pc.vue';
+import accesos_pc from "./ui/bloques/plaza_comidas/Accesos_pc.vue";
+import iluminacion_pc from './ui/bloques/plaza_comidas/Ilum_pc.vue';
+import incendio_pc from './ui/bloques/plaza_comidas/Incendio_pc.vue';
 // ************* SALA VIP ********************
-import vip from '/imports/ui/bloques/sala_vip/Opt_vip.vue';
-import iluminacion_vip from '/imports/ui/bloques/sala_vip/iluminacion_vip.vue';
-import dimmer_vip from '/imports/ui/bloques/sala_vip/dimmer_vip.vue';
-import rgb_vip from '/imports/ui/bloques/sala_vip/rgb_vip.vue';
-import aire_vip from '/imports/ui/bloques/sala_vip/aire_vip.vue';
-import camaras_vip from '/imports/ui/bloques/sala_vip/camaras_vip.vue';
+import vip from './ui/bloques/sala_vip/Opt_vip.vue';
+import iluminacion_vip from './ui/bloques/sala_vip/iluminacion_vip.vue';
+import dimmer_vip from './ui/bloques/sala_vip/dimmer_vip.vue';
+import rgb_vip from './ui/bloques/sala_vip/rgb_vip.vue';
+import aire_vip from './ui/bloques/sala_vip/aire_vip.vue';
+import camaras_vip from './ui/bloques/sala_vip/camaras_vip.vue';
 // ************* AUDITORIO *******************
-import au from '/imports/ui/bloques/auditorio/Opt_au.vue';
-import accesos_au from "/imports/ui/bloques/auditorio/Accesos_au.vue";
-import iluminacion_au from '/imports/ui/bloques/auditorio/Ilum_au.vue';
-import iluminacion_au_a from "/imports/ui/bloques/auditorio/Ilum_au_a.vue";
-import dimmer_auditorios from "/imports/ui/bloques/auditorio/Dim_auditorios.vue";
-import iluminacion_au_b from "/imports/ui/bloques/auditorio/Ilum_au_b.vue";
-import iluminacion_au_prin from "/imports/ui/bloques/auditorio/Ilum_au_prin.vue";
-import aire_au_prin from '/imports/ui/bloques/auditorio/Aire_au_prin.vue';
-import camaras_au_prin from '/imports/ui/bloques/auditorio/Camaras_au_prin.vue'
+import au from './ui/bloques/auditorio/Opt_au.vue';
+import accesos_au from "./ui/bloques/auditorio/Accesos_au.vue";
+import iluminacion_au from './ui/bloques/auditorio/Ilum_au.vue';
+import iluminacion_au_a from "./ui/bloques/auditorio/Ilum_au_a.vue";
+import dimmer_auditorios from "./ui/bloques/auditorio/Dim_auditorios.vue";
+import iluminacion_au_b from "./ui/bloques/auditorio/Ilum_au_b.vue";
+import iluminacion_au_prin from "./ui/bloques/auditorio/Ilum_au_prin.vue";
+import aire_au_prin from './ui/bloques/auditorio/Aire_au_prin.vue';
+import camaras_au_prin from './ui/bloques/auditorio/Camaras_au_prin.vue'
 RouterFactory.configure(router => {
     router.addRoutes([
       {
